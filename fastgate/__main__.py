65e922ffdcb7540a80e924a82087b4cb633e87ff
Davide Depau <davide@depau.eu>
2018-04-12 22:14:21 +0200

    Patch sys.argv in `__main__` to show a meaningful help message on --help

--- fastgate/__main__.py
+++ fastgate/__main__.py
@@ -1,3 +1,13 @@
-from fastgate import cli
+import sys
 
-cli()
+if __name__ == "__main__":
+    from fastgate import cli
+    args = sys.argv[1:]
+
+    this_module = 'fastgate'
+
+    name = 'python -m ' + this_module
+
+    sys.argv = [name] + args
+
+    cli()
